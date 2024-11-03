1882d86bbeb88b46df3ec1b55ab8e8b61bcbcb8c
KunalGupta <kunalgupta100320@gmail.com>
2024-11-04 04:30:07 +0530

    inital project

---
+++ color-fliper/hex.js
@@ -0,0 +1,16 @@
+let hex=[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+let btn=document.getElementById("btn");
+let color=document.querySelector(".color");
+
+btn.addEventListener("click",function(){
+   
+    let hexValue="#";
+    for(let i=0;i<6;i++){
+        hexValue+=hex[randomNumber()];
+        
+    }document.body.style.backgroundColor=hexValue;
+        color.textContent=hexValue
+});
+ function randomNumber(){
+     return Math.floor(Math.random()*hex.length)
+ }                    
