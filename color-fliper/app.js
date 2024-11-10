c5d8779f4ed6a24a1741bbbc0547a0b3b37a4aab
kunalquantummania <119137668+kunalquantummania@users.noreply.github.com>
2024-11-11 04:14:33 +0530

    Update app.js

--- color-fliper/app.js
+++ color-fliper/app.js
@@ -4,6 +4,7 @@ const color= document.querySelector(".color")
  btn.addEventListener("click" ,function(){
     
 const random=Math.floor(Math.random()*colors.length);
+  console.log(colors[random])
     document.body.style.backgroundColor=colors[random];
     color.textContent=colors[random]
  });
